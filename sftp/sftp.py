f379a85dc63d7fb3ba134f16ef8020dbca24d23e
MukeshNarendran <mukesh.narendran@gmail.com>
2023-11-21 12:16:19 +0100

    Update sftp.py
    - edit logging issues
    - download with base folder - download_remote_dir
    - resolve issues with upload_download_file()

--- sftp/sftp.py
+++ sftp/sftp.py
@@ -1,5 +1,5 @@
 # ------------------------------------------------------------------------------
-# Written by Mukesh Narendran 
+# Written by Mukesh Narendran (mukesh.narendran@gmail.com)
 # Connects with sftp server, upload, download folders & files
 # ------------------------------------------------------------------------------
 import os
@@ -17,11 +17,10 @@ def create_log():
         if not os.path.exists(fold):
                 os.mkdir(fold)
         else:
-                print("log folder present")
+                print("log folder already present")
         
         # open logger
-        nm = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-        nm = nm.replace(' ', '_').replace('/', '_')
+        nm = datetime.now().strftime("%d/%m/%Y %H:%M:%S").replace(' ', '_').replace('/', '_')
         log_file = os.path.join(fold, nm+".log")    
         return log_file
 
@@ -32,9 +31,8 @@ logging.basicConfig(
                 format='%(asctime)s:%(levelname)s:%(message)s',
                 level=logging.INFO)
 
-
-class Mysftp:
-        def __init__(self, params):
+class Bovisftp:
+        def __init__(self, params: dict):
                 self.params = params
         
         def connect_sftp(self):
@@ -53,7 +51,7 @@ class Mysftp:
                                 )
                         sft = client.open_sftp()
 
-                        logging.info(f"Connected has been established with {info['host']}")
+                        logging.info(f"Connected has been established with {info['host']}:{info['port']}")
                 except paramiko.AuthenticationException:
                         logging.exception("Authentication failed, please verify your credentials.")
                 except paramiko.SSHException as sshException:
@@ -68,24 +66,38 @@ class Mysftp:
         def download_remote_dir(
                         self, 
                         sft, 
-                        local_dir, 
-                        remote_dir
-                        ):
-                """Recursively download a directory from an SFTP server."""
+                        local_dir: str, 
+                        remote_dir: str,
+                        is_base:bool
+                        ) -> None:
+                """Download directory from the server
+                Args:
+                    sft (_type_): sftp connection
+                    local_dir (str): youir local path to download
+                    remote_dir (str): server path to download
+                    is_base (bool): if True, then it will create a folder with the same name as remote_dir
+                """                             
                 # Ensure the local directory exists
-                os.makedirs(local_dir, exist_ok=True)
+                if is_base:
+                    local_dir = os.path.join(local_dir, os.path.basename(remote_dir))
+                    os.makedirs(local_dir, exist_ok=True)
 
                 # List files and directories in the remote directory
                 for entry in sft.listdir_attr(remote_dir):
                         remote_path = remote_dir + '/' + entry.filename
                         local_path = os.path.join(local_dir, entry.filename)
-
                         if S_ISDIR(entry.st_mode):
-                                self.download_remote_dir(local_path, remote_path)
-                                logging.INFO(f"DOWNLOAD DIR: {remote_dir}")
-                        else:                      
+                                self.download_remote_dir(
+                                        sft, 
+                                        local_path, 
+                                        remote_path,
+                                        is_base=False
+                                        )
+                                logging.info(f"DOWNLOAD DIR: {remote_path}")
+                        else:                   
+                                os.makedirs(os.path.dirname(local_path), exist_ok=True)   
                                 sft.get(remote_path, local_path)
-                                logging.INFO(f"DOWNLOAD FILE: {remote_dir}")
+                                logging.info(f"DOWNLOAD FILE: {remote_path}")
                 return
 
         def upload_local_dir(
@@ -93,8 +105,14 @@ class Mysftp:
                         sft, 
                         local_dir: str, 
                         remote_dir:str
-                        ):
-                """upload file from directory"""
+                        )-> None:
+                """Recursively upload a directory to an SFTP server
+
+                Args:
+                    sft (_type_): sftp connection
+                    local_dir (str): you local path to upload
+                    remote_dir (str): server path to upload
+                """                
 
                 for item in os.listdir(local_dir):
                         local_path = os.path.join(local_dir, item)
@@ -103,17 +121,17 @@ class Mysftp:
                         if os.path.isfile(local_path):
                                 # Upload file
                                 sft.put(local_path, remote_path)
-                                logging.INFO(f"UPLOAD DIR: {local_dir}")
+                                logging.info(f"UPLOAD DIR: {local_dir}")
 
                         else:
                                 # Create remote directory if it doesn't exist
                                 try:
                                         sft.stat(remote_path)
                                 except FileNotFoundError as err:
-                                        logging.INFO(f"FILE CREATED:{err}")
+                                        logging.info(f"FILE CREATED ON SERVER:{err}")
                                         sft.mkdir(remote_path)
 
-                                self.upload_local_dir(local_path, remote_path)
+                                self.upload_local_dir(sft, local_path, remote_path)
                 return
         
         def upload_download_file(
@@ -122,96 +140,56 @@ class Mysftp:
                                 option: str, 
                                 local_filepth: str, 
                                 remote_filepth: str
-                                 ):
+                                 ) -> None:
                 """Upload or download single file from the server
                 Args:
                     option (str): get or push
                 """ 
                 assert isinstance(option, str), "option must be string type"
-                assert (os.path.basename(local_filepth) == os.path.basename(remote_filepth)), "file name must be same"                
-
                 option = option.lower()
                 
                 if option == "get":
+                        # local_filepth = os.path.join(local_filepth, os.path.basename(remote_filepth))
                         sft.get(remote_filepth, local_filepth)
                 elif option == "push":
                         assert os.path.isfile(local_filepth), "local_filepth must be file"
+
+                        remote_filepth = os.path.join(remote_filepth, os.path.basename(local_filepth))
                         try:
                                 sft.put(local_filepth, remote_filepth)
-                                # logging.INFO(f"File uploaded:{local_filepth}")
+                                logging.info(f"File UPLOADED: {local_filepth}")
                         except FileNotFoundError as err:
-                                logging.exception(f"File not uploaded:{err}")
+                                logging.exception(f"FILE NOT UPLOADED:{err}")
                         else:
-                                logging.exception("remote_filepth not found")
+                                logging.exception(f"REMOTE PATH NOT FOUND: {remote_filepth}")
                 else:
-                        logging.exception("Invalid option") 
+                        logging.exception("INVALID OPTION") 
                 return
         
         def listdir_r(
                 self,
                 sft, 
                 remotedir:str, 
-                store_pths: list
                  )-> list:
-                """recursively loop through the folder"""
+                """List all the directories in the server
+
+                Args:
+                    sft (_type_): sftp connection
+                    remotedir (str): remote directory
+
+                Returns:
+                    list: filepaths within directory
+                """                
                 
                 store_pths = []
-                logging.INFO("Listed directories")
+                logging.info("Listing all the directories")
                 for entry in sft.listdir_attr(remotedir):
                         remotepath = remotedir + "/" + entry.filename
                         mode = entry.st_mode
                         if S_ISDIR(mode):
                                 fold, ext = os.path.split(remotepath)
-                                self.listdir_r(remotepath)
+                                self.listdir_r(sft, remotepath)
                                 store_pths.append((ext, fold)) 
                         else:
                                 pass
                 return store_pths
-
-
-if __name__ == "__main__":
-        sftp_info = {
-        "site" :{
-            "host":"xx.xx.xx.xx",
-            "user":"xxxx",
-            "port":xx,
-            "password":"xoxo",
-            },
-        "upload":{
-            "local_dir": "/home/",
-            "remote_dir": "/home",
-
-            },
-        "download":{
-            "local_dir":  "/home/",
-            "remote_dir":"/home/",
-        },
-        "file_up":{
-        }
-    }
-        
-        
-        #grab connection
-        sftp_conn = Mysftp(sftp_info)
-        server = sftp_conn.connect_sftp()
-        upload   = False
-        download = False
-
-        if upload:
-                sftp_conn.upload_local_dir(
-                        sft = server,
-                        local_dir=sftp_info["upload"]["local_dir"],
-                        remote_dir=sftp_info["upload"]["remote_dir"])
-        if download:
-                sftp_conn.download_remote_dir(
-                        sft=server,
-                        local_dir=sftp_info["download"]["local_dir"],
-                        remote_dir=sftp_info["download"]["remote_dir"])
-        
-        #option set/push
-        sftp_conn.upload_download_file(
-                        sft=server,
-                        option="push", 
-                        local_filepth="mc3.mov", 
-                        remote_filepth="mc3.mov",
-                                )
